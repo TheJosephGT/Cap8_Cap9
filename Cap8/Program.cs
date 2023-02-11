c9af05dfddac2ff736c0d5ab40206ab03e54985d
JosephGT <joseph2002610@gmail.com>
2023-02-11 00:30:20 -0400

    first commit

---
+++ Cap8/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+        3 Hacer un programa que muestre la hora
+        del día en formato AM/FM seguida del
+        año, el día y el mes actual.
+        */
+        DateTime tiempo = DateTime.Now;
+
+        System.Console.WriteLine($"Mi tiempo en 24 horas y fecha: {tiempo} {tiempo.ToString("tt")}");
+        Console.WriteLine($"Mi tiempo en 12 horas y fecha: {tiempo.ToString("dd/MM/yyyy hh:mm:ss tt")}");
+
+        /* Hacer un programa que le solicite al
+        usuario dos cadenas y luego las muestre
+        en orden alfabético.
+        */
+
+        string[] cadenas = new string[2];
+
+        for (int i = 0; i < cadenas.Length; i++)
+        {
+            System.Console.Write($"Ingrese la cadena de texto: ");
+            cadenas[i] = Console.ReadLine();
+        }
+        Console.Clear();
+        Array.Sort(cadenas);
+
+        for (int i = 0; i < cadenas.Length; i++)
+        {
+            System.Console.WriteLine($"{i+1}: {cadenas[i]}");
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Cap9/Program.cs
@@ -0,0 +1,75 @@
+﻿internal class Program
+{
+
+    public struct Tienda
+    {
+        public int idProducto;
+        public string nombreProducto;
+        public int precio;
+
+        public Tienda(int idProducto, string nombreProducto, int precio){
+            this.idProducto = idProducto;
+            this.nombreProducto = nombreProducto;
+            this.precio = precio;
+        }
+
+    }
+
+    public struct Dueño
+    {
+        public string nombreDueño;
+
+        public Dueño(string nombreDueño){
+            this.nombreDueño = nombreDueño;
+
+        }
+    }
+
+    public struct Mascota
+    {
+        public string nombreMas;
+        public string raza;
+
+        public Dueño dueño;
+
+        public Mascota(string nombreMas, string raza, string nombreDueño){
+            this.nombreMas = nombreDueño;
+            this.raza = raza;
+
+            dueño = new Dueño(nombreDueño);
+        }
+    }
+
+    enum neumaticos {verano, invierno, asimetrico, radial};
+    private static void Main(string[] args)
+    {
+        /*
+        1. Crear una estructura para guardar los
+        productos de una tienda.
+        */
+
+        Tienda producto = new Tienda(20200447, "Palito de coco", 2000);
+        
+        /*
+        3. Crear estructuras enlazadas para guardar
+        la información de una mascota y su
+        dueño.
+        */
+
+        Mascota mascota = new Mascota("Sara", "Gato", "Glennys");
+
+       /* 4. Crear una enumeración para los
+        diferentes tipos de neumáticos.*/
+
+        neumaticos neu = neumaticos.asimetrico;
+
+        System.Console.WriteLine($"Los neumaticos que me gustan son {neu}");
+
+
+
+
+
+
+
+    }
+}
